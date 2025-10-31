759ed4b14555a7abf6b8d98934c1ef02834db67b
Atharva Angre <107274851+angreatharva@users.noreply.github.com>
2025-10-31 19:22:56 +0530

    app

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const app = express();
 
 const port = 5000;
 
+app.get('/', (req, res) => {
+    res.send('<h2>🚀 Welcome to the Node.js REST API!</h2><p>Use endpoints like /students or /register</p>');
+});
+
 app.use(bodyParser.json({limit:'100mb'}));
 app.use(bodyParser.urlencoded({limit:'100mb',extended:true}));
 
